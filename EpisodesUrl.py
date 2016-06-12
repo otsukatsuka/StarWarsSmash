bcfad9fd4af34927850a24a29247d774a1e931ec
otsukatsuka <tsukacchaaaan@gmail.com>
2016-06-12 19:44:41 +0900

    EpisodesUrl.pyc

--- EpisodesUrl.py
+++
@@ -1,8 +0,0 @@
-EPI1_URL = "http://www.omdbapi.com/?t=Star+Wars&type=movie&y=1999&plot=short&r=json"
-EPI2_URL = "http://www.omdbapi.com/?t=Star+Wars&type=movie&y=2002&plot=short&r=json"
-EPI3_URL = "http://www.omdbapi.com/?t=Star+Wars&type=movie&y=2005&plot=short&r=json"
-EPI4_URL = "http://www.omdbapi.com/?t=Star+Wars&type=movie&y=1977&plot=short&r=json"
-EPI5_URL = "http://www.omdbapi.com/?t=Star+Wars&type=movie&y=1980&plot=short&r=json"
-EPI6_URL = "http://www.omdbapi.com/?t=Star+Wars&type=movie&y=1983&plot=short&r=json"
-EPI7_URL = "http://www.omdbapi.com/?t=Star+Wars&type=movie&y=2015&plot=short&r=json"
-urls = [None,EPI1_URL,EPI2_URL,EPI3_URL,EPI4_URL,EPI5_URL,EPI6_URL,EPI7_URL]
